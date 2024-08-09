24b9914a660392aed5431f05a030704dbbb6100a
chrisblodgett <chris@formax.us>
2024-08-09 03:25:02 -0600

    toggleterm setup stuff

--- lua/chris/plugins/toggleterm.lua
+++ lua/chris/plugins/toggleterm.lua
@@ -1,13 +1,26 @@
 -- toggleterm is a neovim plugin to persist and toggle multiple terminals during an editing session
 -- https://github.com/akinsho/toggleterm.nvim
+--
+--
+-- write some helper functions to run the current python script in a terminal
 local function run_python()
 	vim.cmd.write()
 	print("Running...")
-	local script_path = vim.fn.expand("%:p"):gsub(" ", "\\ ")
-	local command = 'python3 "' .. script_path .. '"'
+	local script_path = vim.fn.expand("%:p"):gsub(" ", "\\ ") -- the vim.fn.expand will take the current folder and escape any spaces
+	-- the %:p means to get the full path of the current file, expand makes it a full path and the : uses it as a modifier
+	-- so we can use gsub to escape any spaces
+	local python_path
+	--check to see if there is a venv and use that python otherwise use the system python
+	if vim.fn.filereadable("venv/bin/python") == 1 then
+		python_path = "venv/bin/python" -- this will make sure the local venv and modules are used
+	else
+		python_path = "python3"
+	end
+	local command = python_path .. " " .. script_path
+	--local command = python_path .. script_path .. '"'
 	require("toggleterm").exec(command)
 end
-
+-- a function to run a command in the terminal
 local function run_command()
 	--prompt the user for the command to run
 	local command = vim.fn.input("Enter command: ")
@@ -32,8 +45,8 @@ return {
 			auto_scroll = true,
 			-- open_mapping = [[<c-\>]],
 		})
-		local keymap = vim.keymap
-		keymap.set("n", "<leadel>tp", run_python, { desc = "Run current current python script in a terminal" })
+		local keymap = vim.keymap -- use this abbreviation for concise code
+		keymap.set("n", "<leadel>tp", run_python, { desc = "Run current file with python in terminal" })
 		keymap.set("n", "<leader>tc", clear_terminal, { desc = "Clear terminal" })
 		keymap.set("n", "<leader>tt", toggle_terminal, { desc = "Toggle terminal" })
 		keymap.set("n", "<leader>tr", run_command, { desc = "Run command in terminal" })
